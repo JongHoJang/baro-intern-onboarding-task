52599c006bc69dbee328519f16a9b14ebbdd728c
jangjh9414 <89680082+JongHoJang@users.noreply.github.com>
2025-02-15 05:54:41 +0900

    add: 처음 로딩 화면 비동기로 인해 부분 제공되지 않는 오류가 있어 인트로페이지 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
-import LoginPage from '@/app/login/page'
+'use client'
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
+  const router = useRouter()
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <LoginPage />
+      <button
+        onClick={() => router.push('/login')}
+        className="text-black px-4 py-2 underline"
+      >
+        프론트엔드 개발 온보딩 과제 (React) 바로가기
+      </button>
     </div>
   )
 }
